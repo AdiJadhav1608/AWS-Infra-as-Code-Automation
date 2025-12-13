7122d6199ef53aaf0031a50d3714a6069184db7e
AdiJadhav1608 <adijadhav8446@gmail.com>
2025-12-13 14:20:42 +0000

    integrate VPC, EC2, and S3 modules into main Terraform configuration

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_name = var.project_name
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = var.bucket_name
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  instance_type = var.instance_type
+  subnet_id     = module.vpc.public_subnet_id
+  vpc_security_group_ids = [module.vpc.sg_id]
+}
