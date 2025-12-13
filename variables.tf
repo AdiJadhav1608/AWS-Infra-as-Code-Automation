c9891eac39f9105dca0e3e75f640cb003445fb4e
AdiJadhav1608 <adijadhav8446@gmail.com>
2025-12-13 14:24:48 +0000

    define global variables for region, instance type, project name, and bucket name

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "project_name" {
+  default = "aditya-terraform-project"
+}
+
+variable "bucket_name" {
+  default = "aditya-terraform-bucket-project"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
